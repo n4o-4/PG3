54db7c293b04927652a5c4b1148225b466728b29
n4o8 <146913762+n4o-4@users.noreply.github.com>
2024-09-30 11:09:43 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <Windows.h>
+#include <stdio.h>
+
+//// Windowsアプリでのエントリーポイント(main関数)
+//int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+//
+//	return 0;
+//}
+
+int main() {
+
+	printf("Hellow PG3 あ");
+
+}
