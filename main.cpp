810fba9793c7ea10e33ad95f20e87525479f8abb
n4o8 <146913762+n4o-4@users.noreply.github.com>
2024-09-30 12:23:27 +0900

    01_01_確認課題

--- main.cpp
+++ main.cpp
@@ -1,14 +1,73 @@
-#include <Windows.h>
 #include <stdio.h>
+#include <iostream>
 
-//// Windowsアプリでのエントリーポイント(main関数)
-//int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-//
-//	return 0;
-//}
+template <typename T1, typename T2>
+
+void Min(T1 a, T2 b) {
+
+	if (typeid(a) == typeid(char) || typeid(b) == typeid(char)) {
+
+		printf("数字以外は代入できません\n");
+		
+		return;
+	}
+
+	if (a > b) {
+		
+		if (typeid(b) == typeid(int))
+		{
+			printf("%d\n", b);
+
+			return;
+		}
+		else if (typeid(b) == typeid(float))
+		{
+			printf("%f\n", b);
+
+			return;
+		}
+		else
+		{
+			printf("%lf\n", b);
+
+			return;
+		}
+	}
+
+
+	else if (a < b) {
+
+		if (typeid(a) == typeid(int))
+		{
+			printf("%d\n", a);
+
+			return;
+		}
+		else if (typeid(a) == typeid(float))
+		{
+			printf("%f\n", a);
+
+			return;
+		}
+		else
+		{
+			printf("%lf\n", a);
+
+			return;
+		}
+	}
+
+	else
+	{
+		printf("同じ値です\n");
+	}
+}
 
 int main() {
 
-	printf("Hellow PG3 あ");
+	Min<int, double>(10, 9.0);
 
+	Min<char, int>('a', 9);
+
+	return 0;
 }
